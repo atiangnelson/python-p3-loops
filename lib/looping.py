72b453359073ce1278060be32722cc7b59313a3b
Nelson <nelson.atiang@student.moringaschool.com>
2025-05-08 09:30:31 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,39 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return[integer**2 for integer in int_list]
+    
 
+
+# def fizzbuzz_printer():
+#     # code goes here!
+#     #x = 1
+#     #while x<=100:
+#     for x in range (1,101):
+#         print(fizzbuzz(x))
+#        # x += 1
+        
+    
 def fizzbuzz():
     # code goes here!
-    pass
+    num =1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print ("FizzBuzz")
+        elif num % 3 == 0:
+            print ("Fizz")
+        elif num % 5 == 0:
+            print ("Buzz")
+        else:
+            print (num)
+        num += 1
+  
+         
